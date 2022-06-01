85ae814407fd89b2dd41e62ef750683a0098b358
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-06-01 07:12:13 +0100

    setting up bad contract

--- contracts/BadContract.sol
+++ contracts/BadContract.sol
@@ -2,6 +2,14 @@
 
 pragma solidity ^0.8.6;
 
+import './GoodContract.sol';
+
 contract BadContract {
-    
+    GoodContract public goodContract; 
+
+    constructor(address _goodContractAddress) {
+        goodContract = GoodContract(_goodContractAddress);
+    }
+
+
 }
