9ffbe24839a9e8fceb538e9c385337f4f66b994c
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-06-01 07:06:43 +0100

    Initial commit

---
+++ contracts/GoodContract.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.6;
+
+contract GoodContract {
+    mapping(address => uint) public balances;
+
+    // update the balances of the caller
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    // send the eth worth of the caller balance back to the caller
+    function withdraw() public {
+        require(balances[msg.sender] > 0, "You have insufficient balance");
+        (bool sent, ) = msg.sender.call{value: balances[msg.sender]}("");
+        require(sent, "Failed to send Ether");
+        // This code won't be executed since the contract balance would have been
+        // drained by the attacker
+        balances[msg.sender] = 0;
+    }
+
+
+}
